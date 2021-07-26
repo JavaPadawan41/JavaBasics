a152be73cb0e1cb661dfad569824068334f7982b
Awesomium40 <Jay@DESKTOP-P6GEI7J>
2021-07-26 15:17:36 -0400

    Add Test Runner/Suite for day 5

---
+++ DayFive/test/com/ss/jb/dayfive/test/TestRunner.java
@@ -0,0 +1,26 @@
+package com.ss.jb.dayfive.test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+/***
+ * Runs unit tests for the Week 1 assignments
+ * @param args
+ */
+public class TestRunner
+{
+
+	public static void main(String[] args)
+	{
+		Result r = JUnitCore.runClasses(TestSuite.class);
+		
+		for (Failure f: r.getFailures())
+		{
+			System.out.println(f.toString());
+		}
+		
+		System.out.println(r.wasSuccessful());
+
+	}
+
+}
---
+++ DayFive/test/com/ss/jb/dayfive/test/TestSuite.java
@@ -0,0 +1,16 @@
+package com.ss.jb.dayfive.test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	DateTimeTest.class,
+	IntJoinTest.class,
+	LambdaTest.class,
+	StringFilterTest.class
+})
+public class TestSuite
+{
+
+}
